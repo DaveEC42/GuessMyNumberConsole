ee45048dd91037dcc304b868c5157c44bfa827c7
Dave Bochichio <dbochichio@gmail.com>
2018-09-15 14:24:20 -0400

    Version 1.0

--- GuessANumber/GuessANumberConsole.cs
+++ GuessANumber/GuessANumberConsole.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿// Simple "Guess My Number" console game
+// Author: Dave Bochichio
+// https://davebochichio.com
+// https://github.com/DaveEC42/GuessMyNumberConsole
+//
+// Version 1.0
+
+using System;
 
 namespace GuessANumber
 {
@@ -15,13 +22,7 @@ namespace GuessANumber
         // Guesses left
         private int guessesLeft;
 
-        // The guess that the player enters
-        private int guess;
-
         Random rand = new Random();
-
-        // When set to true, the player has guessed the number correctly
-        bool winCondition;
 
         // The number that the player has to guess to win the game
         private int winningNumber;
@@ -30,12 +31,10 @@ namespace GuessANumber
         // be easily made; but for this example, we hardcode.
         private void Init()
         {
-            winningNumber = rand.Next(1, 500);
             min = 1;
             max = 500;
+            winningNumber = rand.Next(min, max);
             guessesLeft = 12;
-            guess = 0;
-            winCondition = false;
         }
 
         // Quick introduction to the game
@@ -58,37 +57,43 @@ namespace GuessANumber
             Init();
             // Do the intro
             DoIntro();
+            int guess = 0;
+            bool winCondition = false;
             // Game Loop
             do
             {
                 // Get and then evaluate the guess
-                GetGuess();
-                EvaluateGuess();
-
+                guess = GetGuess();
+                winCondition = EvaluateGuess(guess);
+                guessesLeft--;
             } while (guessesLeft > 0 && !winCondition);
 
             if (guessesLeft == 0 && !winCondition)
             {
                 Console.WriteLine("You ran out of guesses! GAME OVER!");
             }
+            if (winCondition)
+            {
+                Console.WriteLine("Congrats! You win!");
+            }
             Console.ReadKey();
 
         }
   
         // Determine if the guess equaled the winning number, or was greater than or less than
-        private void EvaluateGuess()
+        private bool EvaluateGuess(int guess)
         {
+            bool didPlayerWin = false;
             if (guess == winningNumber)
             {
-                Console.WriteLine("YOU GUESSED CORRECTLY! It took you " + (12 - guessesLeft) + " tries! Congrats!");
-                winCondition = true;
+                didPlayerWin = true;
             }
             else if (guess > winningNumber)
             {
                 Console.WriteLine("You guessed too high! My number is lower!");
                 if (guess < max)
                 {
-                    max = guess;
+                    max = (guess - 1);
                 }
             }
             else if (guess < winningNumber)
@@ -96,18 +101,19 @@ namespace GuessANumber
                 Console.WriteLine("You guessed too low! My number is higher!");
                 if (guess > min)
                 {
-                    min = guess;
+                    min = (guess + 1);
                 }
             }
+            return didPlayerWin;
         }
 
         private int GetGuess()
         {
+            int guess;
             Console.Write("I am thinking of a number from " + min + " to " + max + "! " +
                     "You have " + guessesLeft + " guesses left. Type in your guess!\n");
             string input = Console.ReadLine();
-            guessesLeft--;
-            // If the guess isn't an integer, re-request the guess until it is
+             // If the guess isn't an integer, re-request the guess until it is
             while (!int.TryParse(input, out guess))
             {
                 Console.WriteLine("You must enter an integer as a guess (no decimals)! Try again!");
